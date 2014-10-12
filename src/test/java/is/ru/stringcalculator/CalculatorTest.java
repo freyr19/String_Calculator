802a3501f463feb1fae64b2f5a84dc647e4ad7c7
freyr19 <freyr1980@hotmail.com>
2014-10-12 13:41:09 +0000

    Created first test empty string

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -14,19 +14,5 @@ public class CalculatorTest {
 		assertEquals(0, Calculator.add(""));
 	}
 
-	@Test
-	public void testOneNumber() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-	@Test
-	public void testTwoNumbers() {
-		assertEquals(3, Calculator.add("1,2"));
-	}	
-
-	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
+	
 }
