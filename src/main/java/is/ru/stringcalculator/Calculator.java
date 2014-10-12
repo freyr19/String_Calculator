585fcc644a4738bcb0e3248f116282174ac32ecc
freyr19 <freyr1980@hotmail.com>
2014-10-12 13:47:47 +0000

    Test OneNumber function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,7 +3,11 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		
-		return 0;
+		if(text.equals("")){
+
+			return 0;
+		}
+		return 1;
 	}
+
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -14,5 +14,10 @@ public class CalculatorTest {
 		assertEquals(0, Calculator.add(""));
 	}
 
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
 	
 }
